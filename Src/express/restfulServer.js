7151e15e42f7134df58132174f69823aaa243395
Janitha Tennakoon <janitha000@gmail.comn>
2017-02-12 13:23:07 +0530

    Added restful web service

---
+++ Src/express/restfulServer.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+var user = {
+   "user4" : {
+      "name" : "Lahiru",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
+
+
+app.get('/users', function(req,res){
+  fs.readFile(__dirname + "/express/users.json", 'utf8', function(err,data){
+    console.log(__dirname + "/express/users.json");
+    console.log(data);
+    res.end(data);
+  });
+})
+
+app.post('/users', function(req,res){
+  fs.readFile(__dirname + "/express/users.json", 'utf8', function(err,data){
+    data = JSON.parse(data);
+    data["user4"] = user["user4"];
+    console.log(data);
+    res.end(JSON.stringify(data));
+  });
+})
+
+var server = app.listen(8081, function(){
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+})
